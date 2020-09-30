db47dc05f3207aa00a8e50d8229b0861a481adc7
Alan Bandeira <alan.p.bandeira@gmail.com>
2020-09-29 21:05:19 -0300

    testing deploy with different server config

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
-import path from 'path';
-import express from 'express';
-import serveStatic from 'serve-static';
+//import path from 'path';
+//import express from 'express';
+//import serveStatic from 'serve-static';
 
-const app = express();
-const __dirname = path.resolve();
-app.use(serveStatic(__dirname + "/dist"));
+//const app = express();
+//const __dirname = path.resolve();
+//app.use(serveStatic(__dirname + "/dist"));
 
-const port = process.env.PORT || 5000;
-app.listen(port);
+//const port = process.env.PORT || 5000;
+//app.listen(port);
 
-console.log("App started at port " + port);
+//console.log("App started at port " + port);
+
+var express = require('express');
+var path = require('path');
+var serveStatic = require('serve-static');app = express();
+app.use(serveStatic(__dirname + "/dist"));var port = process.env.PORT || 5000;
+app.listen(port);console.log('server started '+ port);
